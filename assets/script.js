7412141b745304fb5a8b1235f3c518f9d11f7502
TMats <takomats@hotmail.com>
2021-03-18 23:55:19 -0400

    signing off, UV Index function still in works

--- assets/script.js
+++ assets/script.js
@@ -9,6 +9,7 @@ let humidity = document.getElementById('humidity');
 let windSpeed = document.getElementById('wind-speed');
 let uvIndex = document.getElementById('UV-index');
 let forecast = document.getElementsByClassName('forecast');
+let imgEl = document.querySelector('img')
 
 let headingDate = moment().subtract(10, 'days').calendar()
 
@@ -50,9 +51,39 @@ function getApi(city) {
         });
 };
 
+// function getUVApi(data, uvIndex) {
+
+//     // how to get the coordinates from the getApi function API and apply it here to this one call API?
+//     let latitude = data.coord.lat
+//     let longitude = data.coord.lon
+
+//     let oneCallUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + latitude + "&lon=" + longitude + "&appid=7d3afb4e42d64b1033626322feda44f4";
+    
+//     fetch(oneCallUrl)
+//     .then(function (response) {
+//         if (response.ok) {
+//             console.log(response)
+//             response.json().then(function (data) {
+//                 console.log(data)
+//                 displayWeather(data, uvIndex)
+//             })
+//         } else {
+//             alert('Error: ' + response.statusText)
+//         }
+//     })
+//     .catch(function (error) {
+//         alert('Unable to connect to openweathermap');
+//     });
+// }
+
+
 function displayWeather (data, searchCity) {
 
-    cityName.textContent = searchCity + ' ' + '(' + headingDate + ')' + data.weather[0].icon;
+    let iconCode = data.weather[0].icon
+    let iconUrl = "http://openweathermap.org/img/w/" + iconCode + ".png";
+    weatherIcon.setAttribute('src', iconUrl);
+
+    cityName.textContent = searchCity + ' ' + '(' + headingDate + ')';
     temperature.textContent = 'Temperature: ' + data.main.temp + ' °F';
     humidity.textContent = 'Humidity: ' + data.main.humidity + '%';
     windSpeed.textContent = 'Wind Speed: ' + data.wind.speed + ' MPH';
@@ -62,11 +93,3 @@ function displayWeather (data, searchCity) {
 
 // Make a form element in html and make it the parent, then replace cityInput element.
 formElement.addEventListener('submit', formSubmitHandler)
-
-
-// convert Kelvin into fahrenheit.
-// const kelvin = 293;
-// //declaring and assigning celsius variable
-// var celsius = kelvin - 273;
-// console.log(celsius);
-// var fahrenheit = Math.floor(celsius * (9/5) + 32);
